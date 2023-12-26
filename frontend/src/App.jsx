60f04ffa5f81941eb67535f56648a0ec82ddae1f
Iasmin Santos <iasmin.santos.ext@synchro.com.br>
2023-12-26 18:29:32 -0300

    Tela de inicio, FrontEnd

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavPrincipal from './componentes/NavBar'
+import Card from './componentes/Card'
+import './App.css'
+
+
+function App() {
+  return (
+    <div>
+        <NavPrincipal />
+        <Card />
+    </div>
+  )
+}
+
+export default App
